e0ec145e6061e3b749b05dd29108d895dcffcb4e
José Osório <jfrosorio@gmail.com>
2020-05-20 22:13:51 +0100

    chore: packaging improvements

--- dev/app.js
+++ dev/app.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store/';
+import store from '../src/store';
 
-require('./directives/');
+require('../src/directives');
 
 Vue.config.productionTip = false;
 
